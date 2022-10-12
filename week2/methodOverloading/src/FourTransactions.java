ae682fc72e1669817f8544dbeb6c7d55e5e093cd
kadirulge <kadirulge7@gmail.com>
2022-10-12 19:41:17 +0300

    week2-methodOverloading added.

---
+++ week2/methodOverloading/src/FourTransactions.java
@@ -0,0 +1,12 @@
+public class FourTransactions {
+    public int add(int num1, int num2){
+        return num1+num2;
+
+    }
+
+
+    public int add(int num1, int num2, int num3){
+        return num1+num2+num3;
+
+    }
+}
---
+++ week2/methodOverloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FourTransactions fourTransactions = new FourTransactions();
+        System.out.println(fourTransactions.add(2,3));
+        fourTransactions.add(2,3,5);
+    }
+}
