6503466cec85071c89bc902cc793897e2e135d8a
kadirulge <kadirulge7@gmail.com>
2022-10-12 19:07:23 +0300

    week2-reCapDemo_Classes added.

---
+++ week2/reCapDemo_Classes/src/FourTransactions.java
@@ -0,0 +1,14 @@
+public class FourTransactions {
+    public int Add(int sayi1,int sayi2){
+        return sayi1 + sayi2;
+    }
+    public int Sub(int sayi1,int sayi2){
+        return sayi1 - sayi2;
+    }
+    public int Multiply(int sayi1,int sayi2){
+        return sayi1 * sayi2;
+    }
+    public int Divide(int sayi1,int sayi2){
+        return sayi1 / sayi2;
+    }
+}
---
+++ week2/reCapDemo_Classes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FourTransactions fourTransactions = new FourTransactions();
+        int result = fourTransactions.Add(3,4);
+        System.out.println(result);
+    }
+
+}
