5436a888aa935d9c0a6ef2ff6e3664cd67210766
kadirulge <kadirulge7@gmail.com>
2022-10-12 18:35:42 +0300

    week2-methods2 added.

---
+++ week2/methods2/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        String message ="the weather is so nice today.";
+        String newMessage = assignCity();
+        System.out.println(newMessage);
+        int num = sum(15,7);
+        System.out.println(num);
+        int sum = add2(2,3,4);
+        System.out.println(sum);
+    }
+
+    public  static  void add(){
+        System.out.println("Added");
+    }
+
+    public static void delete (){
+        System.out.println("DeleteD");
+    }
+
+    public static void update(){
+        System.out.println("Updated");
+    }
+
+    public static int sum(int num1,int num2){
+        return num1+num2;
+    }
+
+    public static int add2(int... numbers) {
+        int toplam = 0;
+        for (int num:numbers){
+            toplam += num;
+        }
+        return toplam;
+    }
+    public static String assignCity(){
+        return "Ankara";
+    }
+}
